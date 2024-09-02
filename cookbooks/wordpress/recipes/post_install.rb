d44366c1ecec0ef580ab00aa903c4de806f8c337
erikrs92 <erikrs92@users.noreply.github.com>
2024-09-02 16:16:53 -0500

    Mover la instalación de WP Cli, hacia archivo vagrant

--- cookbooks/wordpress/recipes/post_install.rb
+++ cookbooks/wordpress/recipes/post_install.rb
@@ -18,8 +18,12 @@ file '/bin/wp' do
   mode '0755'
 end
 
+
+ 
 # Instalar Wordpress y configurar
-execute 'Finish Wordpress installation' do
-  command 'sudo -u vagrant -i -- wp core install --path=/opt/wordpress/ --url=localhost --title="EPNEWMAN - Herramientas de automatización de despliegues" --admin_user=admin --admin_password="Epnewman123" --admin_email=admin@epnewman.edu.pe'
-  not_if 'wp core is-installed', environment: { 'PATH' => '/bin:/usr/bin:/usr/local/bin' }
-end
+#wp_ip = node['config']['wp_ip'] 
+#execute 'Finish Wordpress installation' do
+#  command "sudo -u vagrant -i -- wp core install --path=/opt/wordpress/ --url='#{wp_ip}' --title='EPNEWMAN - Herramientas de automatización de despliegues' --admin_user=admin --admin_password='Epnewman123' --admin_email=admin@epnewman.edu.pe"
+  
+#  not_if 'wp core is-installed', environment: { 'PATH' => '/bin:/usr/bin:/usr/local/bin' }
+#end
